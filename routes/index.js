d4ac4e5740956c72836576a0fc4cabc24debd2a2
raylee <lijunray@me.com>
2017-05-10 17:44:10 -0400

    add authorized page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ins-auto-like' });
+});
+
+router.get('/authorized', function(req, res, next) {
+  res.render('authorized', { text: 'Thanks for authorization' });
 });
 
 module.exports = router;
