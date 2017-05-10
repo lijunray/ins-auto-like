345c4730fe29a66984b604142a947f383674cb3f
raylee <lijunray@me.com>
2017-05-10 18:00:31 -0400

    fix

--- routes/authorized.js
+++ routes/authorized.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/authorized', function(req, res, next) {
-  res.render('authorized', { text: 'Thanks for authorization' });
+    res.send('respond with a resource');
 });
 
 module.exports = router;
