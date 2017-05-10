f44d55cb1215e50f1b450b4c2f177f17cc202e79
raylee <lijunray@me.com>
2017-05-10 18:25:26 -0400

    change text to title

--- routes/authorized.js
+++ routes/authorized.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('authorized', { text: 'Thanks for authorization' });
+  res.render('authorized', { title: 'Thanks for authorization' });
 });
 
 module.exports = router;
